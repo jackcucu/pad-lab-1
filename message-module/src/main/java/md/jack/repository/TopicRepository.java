d942ffd9c708b9405b03340e9185183db4315b8c
Cucu Eugeniu <jackcucu@gmail.com>
2017-09-21 21:36:00 +0300

    Add repositories

---
+++ message-module/src/main/java/md/jack/repository/TopicRepository.java
@@ -0,0 +1,10 @@
+package md.jack.repository;
+
+import md.jack.model.db.Topic;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TopicRepository extends JpaRepository<Topic, Long>
+{
+}
