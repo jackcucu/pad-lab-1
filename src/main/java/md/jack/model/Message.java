ff344913dfa0bb35fb59ccdc138de49e9a6bbb45
Cucu Eugeniu <eugeniu.cucu@atomate.net>
2017-09-27 09:44:50 +0300

    Add model

---
+++ src/main/java/md/jack/model/Message.java
@@ -0,0 +1,28 @@
+package md.jack.model;
+
+public class Message
+{
+    private String name;
+
+    private String payload;
+
+    public String getName()
+    {
+        return name;
+    }
+
+    public void setName(final String name)
+    {
+        this.name = name;
+    }
+
+    public String getPayload()
+    {
+        return payload;
+    }
+
+    public void setPayload(final String payload)
+    {
+        this.payload = payload;
+    }
+}
